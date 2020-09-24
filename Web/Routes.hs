68e4c7812d247a9cba3c34ba2090515434d14c43
Montmorency <lamberh@tcd.ie>
2020-09-24 16:01:33 +0100

    Added License

--- Web/Routes.hs
+++ Web/Routes.hs
@@ -5,12 +5,16 @@ import Web.Types
 
 -- Generator Marker
 instance AutoRoute CoffeesController
-type instance ModelControllerMap WebApplication Coffee = CoffeesController
 
 instance AutoRoute UsersController
-type instance ModelControllerMap WebApplication User = UsersController
 
 instance AutoRoute CommentsController
-type instance ModelControllerMap WebApplication Comment = CommentsController
 
 instance AutoRoute StaticController
+
+type instance ModelControllerMap WebApplication Coffee = CoffeesController
+
+type instance ModelControllerMap WebApplication User = UsersController
+
+type instance ModelControllerMap WebApplication Comment = CommentsController
+
